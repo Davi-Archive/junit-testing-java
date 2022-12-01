bfc13467c6a7f6875134ad5c978d40410e04f21b
d1av <davi4alves@gmail.com>
2022-12-01 16:34:30 -0300

    feat: add assertThat method

--- test001/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ test001/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,5 +1,10 @@
 package br.ce.wcaquino.servicos;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import java.util.Date;
 
 import org.junit.Assert;
@@ -24,9 +29,16 @@ public class LocacaoServiceTest {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 
 		// verificacao
+
+		assertThat(locacao.getValor(), is(equalTo(5.0)));
+		assertThat(locacao.getValor(), is(not(6.0)));
+
+		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+
 		Assert.assertEquals(locacao.getValor(), 5.0, 0.01);
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
-	
+
 }
